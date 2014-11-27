63858cd640bd60ad95ffcf0553fd3cccf2f03727
Nick Bray <ncbray@gmail.com>
2014-11-27 09:45:26 -0800

    Rename regenerate main.

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ set -e
 echo "### Cleaning directory"
 rm -r src/generated 2> /dev/null || true
 echo "### Generating sources"
-go run src/tools/regenerate/regenerate_main.go
+go run src/tools/regenerate/main.go
 echo "### Running tests"
 go test ./...
